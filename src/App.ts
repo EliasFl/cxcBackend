9370f5772e87abf04fa5fb2370c1155e26121b8e
Elias <eliasflores13@outlook.com>
2020-04-08 00:05:05 -0700

    Added an asyncronous function to start the server

--- src/App.ts
+++ src/App.ts
@@ -5,60 +5,62 @@ import { Request, Response } from "express";
 import * as express from "express";
 import { Routes } from "./routes";
 
-createConnection()
-  .then(async connection => {
-    //Initializes express server
-    const app = express();
-    app.use(bodyParser.json());
-    app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
+async function startServer() {
+  try {
+    const connection = await createConnection()
+  } catch (error) {
+    console.log(`ERROR: ${error}`)
+  }
+  const app = express();
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
 
-      // authorized headers for preflight requests
-      // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+
+    app.options("*", (req, res) => {
+      // allowed XHR methods
       res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Access-Control-Allow-Methods",
+        "GET, PATCH, PUT, POST, DELETE, OPTIONS"
       );
-      next();
+      res.send();
+    });
+  });
 
-      app.options("*", (req, res) => {
-        // allowed XHR methods
-        res.header(
-          "Access-Control-Allow-Methods",
-          "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+  // register express routes from defined application routes
+  Routes.forEach(route => {
+    (app as any)[route.method](
+      route.route,
+      (req: Request, res: Response, next: Function) => {
+        const result = new (route.controller as any)()[route.action](
+          req,
+          res,
+          next
         );
-        res.send();
-      });
-    });
-
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-      (app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
+        if (result instanceof Promise) {
+          result.then(result =>
+            result !== null && result !== undefined
+              ? res.send(result)
+              : undefined
           );
-          if (result instanceof Promise) {
-            result.then(result =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
-            );
-          } else if (result !== null && result !== undefined) {
-            res.json(result);
-          }
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
         }
-      );
-    });
+      }
+    );
+  });
 
-    const port = process.env.PORT || 8080
-    app.listen(port);
+  const port = process.env.PORT || 8080
+  app.listen(port);
 
-    console.log(`Server has started at port ${port}`);
-  })
-  .catch(error =>
-    console.log(`An error has been encountered starting the server: ${error}`)
-  );
+  console.log(`Server has started at port ${port}`);
+}
+
+startServer()
